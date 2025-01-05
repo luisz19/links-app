4c84e9b43ef9276c320545271620dba3b1e453bf
Luis <luiss18.code@gmail.com>
2025-01-04 22:45:52 -0300

    configuração da estilização

---
+++ src/app/index/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+import { styles } from "./styles"
+
+export default function Index() {
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}> Hello React Native </Text>
+            <Text style={styles.title}> Hello React Native </Text>
+        </View>
+    ) 
+    
+}
+
