56ca30067fa4b307931e396604b3ee8b506a0fc9
Luis <luiss18.code@gmail.com>
2025-01-08 17:01:43 -0300

    criado modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View, TouchableOpacity, FlatList } from "react-native"
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { colors } from "@/styles/colors"
@@ -37,8 +37,32 @@ export default function Index() {
                 style={styles.links}
                 contentContainerStyle={styles.linksContent}
                 showsVerticalScrollIndicator={false}
-            
             />
+
+            <Modal transparent visible>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+                            <TouchableOpacity>
+                                <MaterialIcons name="close" size={20} color={colors.gray[400]} />   
+                            </TouchableOpacity>
+                             
+
+                        </View>
+
+                        <Text style={styles.modalLinkName}>
+                            Rocketseat
+                        </Text>
+
+                        <Text style={styles.modalUrl}>https://app.rocketseat.com.br/</Text>
+
+                    </View>
+
+                </View>
+
+
+            </Modal>
         </View>
     ) 
     
